57df32b2e098ea1ee43bff07a362f0ebdb57e367
Prin'Meshia <vallee.jonathan@outlook.com>
2016-05-25 16:58:54 +0200

    0.0.2
    Add animate function

--- owl.js
+++ owl.js
@@ -1,7 +1,7 @@
 /**
  * Created by Prin'Meshia.
  * github ; https://github.com/PrinMeshia/owljs
- * Version : 0.0.1
+ * Version : 0.0.2
  * created : May 2016
  * Updated : May 2016
  *
@@ -13,11 +13,30 @@
     var Owljs = function (elem) {
         this.e = document.querySelectorAll(elem);
         this.elength = this.e.length;
-        this.version = '0.0.1';
-        this.github = 'https://github.com/PrinMeshia/owljs'
+        var version = '0.0.2';
+        var github = 'https://github.com/PrinMeshia/owljs';
+
+        /*ANIMATE*/
+        this.animation = {
+            linear: function (progress) {
+                return progress;
+            }
+        }
         return this;
     };
     Owljs.prototype = {
+        val: function (newval) {
+            for (var i = 0; i < this.elength; i++) {
+                this.e[i].value = newval;
+            }
+            return this;
+        },
+        css: function (css, val) {
+            for (var i = 0; i < this.elength; i++) {
+                this.e[i].style[css] = val;
+            }
+            return this;
+        },
         hide: function () {
             for (var i = 0; i < this.elength; i++) {
                 this.e[i].style.display = 'none';
@@ -40,21 +59,94 @@
             }
             return this;
         },
-        css: function (css, val) {
+        toggleClass: function () {
             for (var i = 0; i < this.elength; i++) {
-                this.e[i].style[css] = val;
+                if (this.e[i].style.display !== 'none') {
+                    this.e[i].style.display = 'none';
+                } else {
+                    this.e[i].style.display = 'inherit';
+                }
             }
             return this;
         },
-        val: function (newval) {
-            for (var i = 0; i < this.elength; i++) {
-                this.e[i].value = newval;
-            }
-            return this;
+        /**
+         * params :
+         *          - t : duration
+         *          - d : dif
+         *          - c : complete function
+         *          - s : step
+         **/
+        animate: function (params) {
+            var dateS = new Date;
+            var animeInt = setInterval(function () {
+                var timeInt = new Date - dateS;
+                var progress = timeInt / params.t;
+                if (progress > 1) {
+                    progress = 1;
+                }
+                params.progress = progress;
+                var delta = params.d(progress);
+                params.s(delta);
+                if (progress == 1) {
+                    clearInterval(animeInt);
+                    params.c();
+                }
+            }, params.delay || 10);
+        },
+        fadeIn: function (params) {
+            var to = 0;
+            var _this = this;
+            this.animate({
+                t: params.t,
+                d: function (progress) {
+                    progress = this.progress;
+                    return _this.animation.linear(progress);
+                },
+                c: function () {
+                    for (var i = 0; i < _this.elength; i++) {
+                        _this.e[i].style.opacity = '';
+                    }
+                },
+                s: function (delta) {
+                    for (var i = 0; i < _this.elength; i++) {
+
+                        _this.e[i].style.opacity = to + delta;
+                        _this.e[i].style.display = 'inherit';
+                    }
+                }
+            });
+        },
+        fadeOut: function (params) {
+            var to = 1;
+            var _this = this;
+            this.animate({
+                t: params.t,
+                d: function (progress) {
+                    progress = this.progress;
+                    return _this.animation.linear(progress);
+                },
+                c: function () {
+                    for (var i = 0; i < _this.elength; i++) {
+                        _this.e[i].style.display = 'none';
+                        _this.e[i].style.opacity = '';
+                    }
+                },
+                s: function (delta) {
+                    for (var i = 0; i < _this.elength; i++) {
+                        _this.e[i].style.opacity = to - delta;
+                    }
+                }
+            });
         }
     };
     if (!window.owl) {
         window.owl = owl;
+    }
+    if (!window.requestAnimationFrame) {
+        window.requestAnimationFrame = function (fn) {
+            var timer = 16.66; // 60 fps
+            setTimeout(fn, timer);
+        }
     }
 })(window);
 
