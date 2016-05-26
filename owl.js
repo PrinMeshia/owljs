ad9cd9bc68b2287f29871afcc8b4979782c41063
Prin'Meshia <vallee.jonathan@outlook.com>
2016-05-26 09:24:39 +0200

    0.0.3
    Add class Action
    
    Block new fadeAction when a fade action is launched

--- owl.js
+++ owl.js
@@ -15,7 +15,7 @@
         this.elength = this.e.length;
         var version = '0.0.2';
         var github = 'https://github.com/PrinMeshia/owljs';
-
+        this.fadeStart = false;
         /*ANIMATE*/
         this.animation = {
             linear: function (progress) {
@@ -37,6 +37,10 @@
             }
             return this;
         },
+
+        /*
+         * SHOW - HIDE - TOGGLZ
+         */
         hide: function () {
             for (var i = 0; i < this.elength; i++) {
                 this.e[i].style.display = 'none';
@@ -59,17 +63,48 @@
             }
             return this;
         },
-        toggleClass: function () {
+        /*
+         * CLASS ACTION
+         */
+        hasClass: function (el, className) {
+            if (el.classList)
+                return el.classList.contains(className);
+            else
+                return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
+        },
+        addClass: function (className) {
             for (var i = 0; i < this.elength; i++) {
-                if (this.e[i].style.display !== 'none') {
-                    this.e[i].style.display = 'none';
-                } else {
-                    this.e[i].style.display = 'inherit';
+                if (this.e[i].classList)
+                    this.e[i].classList.add(className);
+                else if (!this.hasClass(this.e[i], className))
+                    this.e[i].className += " " + className;
+            }
+            return this;
+        },
+        removeClass: function (className) {
+            for (var i = 0; i < this.elength; i++) {
+                if (this.e[i].classList)
+                    this.e[i].classList.remove(className);
+                else if (this.hasClass(this.e[i], className)) {
+                    var reg = new RegExp('(\\s|^)' + className + '(\\s|$)');
+                    this.e[i].className = this.e[i].className.replace(reg, ' ');
                 }
             }
             return this;
         },
+        toggleClass: function (className) {
+            for (var i = 0; i < this.elength; i++) {
+                if (this.hasClass(this.e[i], className)) {
+                    this.e[i].classList.remove(className);
+                } else {
+                    this.e[i].classList.add(className);
+                }
+            }
+            return this;
+        },
+
         /**
+         * ANIMATE
          * params :
          *          - t : duration
          *          - d : dif
@@ -92,51 +127,67 @@
                     params.c();
                 }
             }, params.delay || 10);
+            return this;
         },
         fadeIn: function (params) {
-            var to = 0;
-            var _this = this;
-            this.animate({
-                t: params.t,
-                d: function (progress) {
-                    progress = this.progress;
-                    return _this.animation.linear(progress);
-                },
-                c: function () {
-                    for (var i = 0; i < _this.elength; i++) {
-                        _this.e[i].style.opacity = '';
-                    }
-                },
-                s: function (delta) {
-                    for (var i = 0; i < _this.elength; i++) {
+            if (!fadeStart) {
+                this.fadeStart = true;
 
-                        _this.e[i].style.opacity = to + delta;
-                        _this.e[i].style.display = 'inherit';
+                var to = 0;
+                var _this = this;
+                this.animate({
+                    t: params.t,
+                    d: function (progress) {
+                        progress = this.progress;
+                        return _this.animation.linear(progress);
+                    },
+                    c: function () {
+                        for (var i = 0; i < _this.elength; i++) {
+                            _this.e[i].style.opacity = '';
+                        }
+                        _this.fadeStart = false;
+                    },
+                    s: function (delta) {
+                        for (var i = 0; i < _this.elength; i++) {
+                            if (_this.e[i].offsetWidth <= 0 || _this.e[i].offsetHeight <= 0) {
+                                _this.e[i].style.opacity = to + delta;
+                                _this.e[i].style.display = 'inherit';
+                            }
+                        }
                     }
-                }
-            });
+                });
+            }
+
+            return this;
         },
         fadeOut: function (params) {
-            var to = 1;
-            var _this = this;
-            this.animate({
-                t: params.t,
-                d: function (progress) {
-                    progress = this.progress;
-                    return _this.animation.linear(progress);
-                },
-                c: function () {
-                    for (var i = 0; i < _this.elength; i++) {
-                        _this.e[i].style.display = 'none';
-                        _this.e[i].style.opacity = '';
+            if (!fadeStart) {
+                this.fadeStart = true;
+                var to = 1;
+                var _this = this;
+                this.animate({
+                    t: params.t,
+                    d: function (progress) {
+                        progress = this.progress;
+                        return _this.animation.linear(progress);
+                    },
+                    c: function () {
+                        for (var i = 0; i < _this.elength; i++) {
+                            _this.e[i].style.display = 'none';
+                            _this.e[i].style.opacity = '';
+
+                        }
+                        _this.fadeStart = false;
+                    },
+                    s: function (delta) {
+                        for (var i = 0; i < _this.elength; i++) {
+                            if (_this.e[i].offsetWidth > 0 || _this.e[i].offsetHeight > 0)
+                                _this.e[i].style.opacity = to - delta;
+                        }
                     }
-                },
-                s: function (delta) {
-                    for (var i = 0; i < _this.elength; i++) {
-                        _this.e[i].style.opacity = to - delta;
-                    }
-                }
-            });
+                });
+            }
+            return this;
         }
     };
     if (!window.owl) {
